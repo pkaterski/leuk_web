7a9fa17b6e8c6cfac72590509411532ce955555c
Parashkev Katerski <pkaterski@gmail.com>
2023-01-29 16:18:56 +0200

    update prepare script

--- prepare_archive.sh
+++ prepare_archive.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-zip -r out.zip dist index.html style.css
+OUTFILE=leukemia_browser.zip
+zip -r $OUTFILE dist index.html style.css
 echo "-----------------------------"
 echo "moving output file to Desktop"
-mv out.zip ~/Desktop/
+mv $OUTFILE ~/Desktop/
 echo "done."
