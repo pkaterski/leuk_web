85227c848b276f1de92ab05b6665e3389c1937de
Parashkev Katerski <pkaterski@gmail.com>
2023-01-29 10:54:41 +0200

    better visualize data

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+// get html elements
+const elements = {};
+elements.redBloodCellcount = document.getElementById("red-blood-cell-count");
+elements.whiteBloodCellCount = document.getElementById("white-blood-cell-count");
+elements.thrombocytesCount = document.getElementById("thrombocytes-count");
+elements.aggressiveLeukemiaCellCount = document.getElementById("aggressive-leukemia-cell-count");
+elements.nonAggressiveLeukemiaCellCount = document.getElementById("non-aggressive-leukemia-cell-count");
+elements.drugInAction = document.getElementById("drug-in-action");
+elements.isAlive = document.getElementById("is-alive");
+elements.criticalTime = document.getElementById("critical-time");
 
-const mySpan = document.getElementById("data");
+
 let bvs = beginBVs;
 let timePassed = 0;
 const TIME_INTERVAL_MS = 500;
 
+const updateHTMLValues = (bvs) => {
+  elements.redBloodCellcount.innerHTML = bvs.whiteBloodCells;
+  elements.whiteBloodCellCount.innerHTML = bvs.redBloodCells;
+  elements.thrombocytesCount.innerHTML = bvs.thrombocytes;
+  elements.aggressiveLeukemiaCellCount.innerHTML = bvs.aggressiveLeukemiaCells;
+  elements.nonAggressiveLeukemiaCellCount.innerHTML = bvs.nonAggressiveLeukemiaCells;
+  elements.drugInAction.innerHTML = bvs.drug;
+  elements.isAlive.innerHTML = bvs.alive;
+};
+
+updateHTMLValues(bvs);
+
 setInterval(() => {
   timePassed += TIME_INTERVAL_MS
   bvs = handleIter(bvs, timePassed)
-  mySpan.innerHTML = JSON.stringify(bvs, null, 2)
-}, TIME_INTERVAL_MS)
 
-console.log()
+  updateHTMLValues(bvs);
+}, TIME_INTERVAL_MS)
