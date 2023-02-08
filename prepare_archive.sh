39a01ef5687970b2bf253bdba59d062d4417522a
Parashkev Katerski <pkaterski@gmail.com>
2023-02-08 22:46:52 +0200

    prepare vanilla react+vite project

--- prepare_archive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-OUTFILE=leukemia_browser.zip
-zip -r $OUTFILE dist index.html style.css
-echo "-----------------------------"
-echo "moving output file to Desktop"
-mv $OUTFILE ~/Desktop/
-echo "done."
