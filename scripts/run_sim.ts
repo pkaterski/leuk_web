768395dfea7e9446869aad10bb6f4281d3432cb3
Parashkev Katerski <pkaterski@gmail.com>
2023-09-27 20:49:52 +0300

    fix ts script

--- scripts/run_sim.ts
+++ scripts/run_sim.ts
@@ -1,1 +1,35 @@
-// todo
+import { initSimParams } from "../src/initParams";
+import { PatientState } from "../src/leukLogic/initHealthy";
+import { beginBVs, handleIter } from "../src/leukLogic/leuk";
+
+import * as fs from 'fs'
+
+const TIME_INTERVAL = 100
+
+let bvss: PatientState[] = []
+let ts: number[] = []
+let currentTime = 0
+let currentBvs = beginBVs
+
+for (let i = 0; i < 500; i++) {
+  currentTime += i / TIME_INTERVAL
+  const result = handleIter(currentBvs, currentTime, undefined, initSimParams)
+  currentBvs = result
+
+  bvss.push(result)
+  ts.push(currentTime)
+}
+
+let data = ''
+data += bvss.map(bvs => bvs.whiteBloodCells + bvs.aggressiveLeukemiaCells + bvs.nonAggressiveLeukemiaCells).join(',')
+data += '\n'
+data += bvss.map(bvs => bvs.whiteBloodCells).join(',')
+data += '\n'
+data += bvss.map(bvs => bvs.stemCells).join(',')
+data += '\n'
+data += bvss.map(bvs => bvs.aggressiveLeukemiaCells).join(',')
+data += '\n'
+data += bvss.map(bvs => bvs.nonAggressiveLeukemiaCells).join(',')
+data += '\n'
+
+fs.writeFileSync('test.csv', data)
