8b4f7b41f7746fa320b6d97a65253cb0a9570a0a
Parashkev Katerski <pkaterski@gmail.com>
2023-09-27 19:58:05 +0300

    add approaching lines drawing

---
+++ scripts/approaching_lines.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import math
+
+
+
+Ms=2
+ms=1
+
+k=0.04
+
+xss = []
+yss = []
+
+mul=10
+for i in range(-1*mul,4*mul+1):
+    xs = []
+    ys = []
+    #initial param
+    I = i/mul
+    y = I
+    xs.append(0)
+    ys.append(y)
+
+    inside=False
+
+    for t in range(1,100):
+        if y > Ms:
+            y = -k*t + I
+        elif y < ms:
+            y = k*t + I
+        else:
+            if not inside:
+                c = (y - (Ms+ms)/2) / math.exp(2*k*(t-1)/(ms-Ms))
+                inside=True
+            y = c * math.exp(2*k*t/(ms-Ms)) + (Ms+ms)/2
+        xs.append(t)
+        ys.append(y)
+    xss.append(xs)
+    yss.append(ys)
+
+#plt.axhline(y = Ms, color = 'gray', linestyle = '--', linewidth=0.9)
+#plt.axhline(y = ms, color = 'gray', linestyle = '--', linewidth=0.9)
+
+for i in range(len(xss)):
+    xs=xss[i]
+    ys=yss[i]
+    plt.plot(xs,ys,'-', linewidth=1)
+plt.show()
+
+
+# ---------------------------------------
+# Euilers method sin(x)
+
+#s = 0
+#data = []
+#ts = []
+#
+#delta = .01
+#for i in range(15000):
+#    s += delta * math.cos(i * delta)
+#    s *= 1.0005
+#    data.append(s)
+#    ts.append(i * delta)
+#
+#plt.plot(ts, data)
+#plt.show()
+
+
+
